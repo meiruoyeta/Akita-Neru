6de39d559c54879e77c6f570dff0b8f1469f6a43
Kayra <devhelen@tutanota.com>
2026-07-14 20:57:58 +0300

    index.js'nin configs/main.json yolunu bulması için index.js içinde config değişkeni tanımladım.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { Client, GatewayIntentBits } from 'discord.js';
+import config from "./configs/main.json" with { type: "json" };
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
 
 client.on('ready', () => {
   console.log(`${client.user.tag} olarak giriş yapıldı`);
 });
 
-client.login(TOKEN);
+client.login(config.TOKEN);
