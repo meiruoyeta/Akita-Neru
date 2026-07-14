4b65c8bc5184d706e23a84cc32bc6fe160e6d9b7
Kayra <devhelen@tutanota.com>
2026-07-14 20:37:37 +0300

    temel index komutunu yazdım. belki ileride projeyi ingilizceye çevirebiliriz.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-test
+import { Client, GatewayIntentBits } from 'discord.js';
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} olarak giriş yapıldı`);
+});
+
+client.login(TOKEN);
